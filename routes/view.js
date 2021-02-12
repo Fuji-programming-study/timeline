b6f1383917704cef302b65bbed649f188324a625
Syogo <studyfuji043@gmail.com>
2021-02-12 11:47:53 +0900

    view.jsにデータベースに保存する機能を追加

--- routes/view.js
+++ routes/view.js
@@ -1,14 +1,26 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const uuid = require('uuid');
+const Content = require('../models/content');
 
 router.get('/', (req, res, next) => {
   res.render('view');
 });
 
 router.post('/', (req, res, next) => {
-  console.log(req.body); // TODO コンテンツを保存する実装をする
-  res.redirect('/');
+  const contentId = uuid.v4();
+  const updatedAt = new Date();
+  Content.create({
+    contentId: contentId,
+    contentTitle: req.body.contentTitle.slice(0, 225) || '(名称未設定)',
+    contentType: req.body.contentType.slice(0, 225) || '(名称未設定)',
+    contentStartDate:req.body.contentStartDate.slice(0, 225) || '(名称未設定)',
+    contentEndDate: req.body.contentEndDate.slice(0, 225) || '(名称未設定)',
+    updatedAt
+  }).then((content) => {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
